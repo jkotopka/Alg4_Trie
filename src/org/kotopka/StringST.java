c6b96d50514bf2e2260e206a2905a6e551edd73b
John Kotopka <jkotopka@gmail.com>
2021-04-06 10:41:26 -0500

    add StringST.java class skeleton

---
+++ src/org/kotopka/StringST.java
@@ -0,0 +1,55 @@
+package org.kotopka;
+
+/**
+ * {@code StringST} - Trie-based symbol table with {@code String} keys. Adapted from <a href="https://algs4.cs.princeton.edu/home/">Altorithms 4th ed</a> by Robert
+ * Sedgewick and Kevin Wayne.
+ * @param <Value> Generic value associated with the {@code String} key
+ */
+
+public class StringST<Value> {
+
+    public StringST() {
+
+    }
+
+    public void put(String key, Value val) {
+
+    }
+
+    public Value get(String key) {
+
+    }
+
+    public void delete(String key) {
+
+    }
+
+    public boolean contains(String key) {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public String longestPrefixOf(String s) {
+
+    }
+
+    public Iterable<String> keysWithPrefix(String s) {
+
+    }
+
+    public Iterable<String> keysThatMatch(String s) {
+
+    }
+
+    public int size() {
+
+    }
+
+    public Iterable<String> keys() {
+
+    }
+
+}
