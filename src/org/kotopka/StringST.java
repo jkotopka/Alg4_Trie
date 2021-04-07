64bc6cfba6c06095a94035c7a80631cedb9833cc
John Kotopka <jkotopka@gmail.com>
2021-04-07 09:29:44 -0500

    update StringST.java, implement longestPrefixOf(), search()

--- src/org/kotopka/StringST.java
+++ src/org/kotopka/StringST.java
@@ -64,7 +64,16 @@ public class StringST<Value> {
     public boolean isEmpty() { return size == 0; }
 
     public String longestPrefixOf(String s) {
+        int length = search(root, s, 0, 0);
+        return s.substring(0, length);
+    }
 
+    private int search(Node x, String s, int d, int length) {
+        if (x == null) return length;
+        if (x.val != null) length = d;
+        if (d == s.length()) return length;
+        char c = s.charAt(d);
+        return search(x.next[c], s, d + 1, length);
     }
 
     public Iterable<String> keys() {
@@ -85,8 +94,24 @@ public class StringST<Value> {
         }
     }
 
-    public Iterable<String> keysThatMatch(String s) {
+    public Iterable<String> keysThatMatch(String pattern) {
+        Queue<String> queue = new ArrayDeque<>();
+        collect(root, "", pattern, queue);
+        return queue;
+    }
 
+    private void collect(Node x, String prefix, String pattern, Queue<String> queue) {
+        int d = prefix.length();
+        if (x == null) return;
+        if (d == pattern.length() && x.val != null) queue.add(prefix);
+        if (d == pattern.length()) return;
+
+        char next = pattern.charAt(d);
+        for (int c = 0; c < R; c++) {
+            if (next == '.' || next == c) {
+                collect(x.next[c], prefix + c, pattern, queue);
+            }
+        }
     }
 
     public int size() { return size; }
